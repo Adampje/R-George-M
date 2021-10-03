93c26677590765ad909693eee59f514ef88858a8
MieuwPieuw <326758@student.mboutrecht.nl>
2021-10-03 23:40:53 +0200

    Booking page sprint1
    De basis van de booking page.

---
+++ bookingpage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<h1 STYLE="font-size: 35px; font-family: , 'Reservations', Times, serif;">Book event</h1>
+
+<img src='img/GeorgeMarina2.png' style='position:absolute; top:0; right:0;' width='100' height='100' alt='logoo'>
+
+  <hr style="height:35px;border-width:0;color:black;background-color:lightgrey">
+  <hr style="height:2px;border-width:0;color:black;background-color:black">
+
+<p> In search of an inspiring, creative meeting space that's all about quality,</p>
+<p>comfort and a relaxed atmosphere? We're here to make it happen for you. </p>
+<p>With our great and well trained best of the best staff, we from George Marina </p>
+<p>can promise you a guaranteed succesful fancy lunch or dinner.</p>
+  <link rel="stylesheet" href="booking.css">
+
+<a href= "contact.php">Contact</a>
+
+</head>
+</html>
---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<h1 STYLE="font-size: 35px; font-family: , 'Reservations', Times, serif;">Book event</h1>
+
+<img src='img/GeorgeMarina2.png' style='position:absolute; top:0; right:0;' width='100' height='100' alt='logoo'>
+
+  <hr style="height:35px;border-width:0;color:black;background-color:lightgrey">
+  <hr style="height:2px;border-width:0;color:black;background-color:black">
+
+  <h2>phone number: </h2>
+  <h3>020 737 0280</h3>
+  <h2>Mail: </h2>
+  <h3>hello@GeorgeMarina.nl</h3> 
+    <h2> of contacteer ons:</h2>
+    <form action="/action_page.php">
+  <label for="phonenumber">Uw telefoon nummer:</label><br>
+  <input type="text" id="phonenumber" name="phonenumber" value="Number"><br>
+  <label for="lname"></label><br>
+  <input type="submit" value="Submit">
+</form> 
+  <link rel="stylesheet" href="booking.css">
