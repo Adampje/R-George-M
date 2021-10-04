71770fb4ba345e5a70ea0387288f3fc729e42573
InfernoMa <75612582+InfernoMa@users.noreply.github.com>
2021-10-04 16:06:01 +0200

    Added: Google maps

--- Restaurant George Marina/contact.php
+++ Restaurant George Marina/contact.php
@@ -1,17 +1,13 @@
 <div class="contact-text">
 <h3 >Contact</h3>
-
+<small style=" font-family:'Chalkboard SE', 'Comic Neue', sans-serif;;"> Elke dag geopend vanaf 10:00 tot 00:00 </small>
 <h3>_________________________</h3>
 
 <h4>E-mail: contact@georgemarina.nl</h4>
 <h4>Tel: 020-1234567</h4>
 
-    <form action="https://www.google.com/maps/place/GEORGE+MARINA/@52.3419796,4.9165536,17z/data=!3m1!4b1!4m5!3m4!1s0x47c60bd4c75894bd:0xcdcb9a745b29464d!8m2!3d52.341996!4d4.9188254">
-<h4>address: <input type="submit" value="Maps" />
-    </form>
-</h4>
-
-<small style=" font-family:'Chalkboard SE', 'Comic Neue', sans-serif;;"> Elke dag geopend vanaf 10:00 tot 00:00 </small>
-
+<div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.5681755067185!2d4.91655361580083!3d52.3419795797809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c60bd4c75894bd%3A0xcdcb9a745b29464d!2sGEORGE%20MARINA!5e0!3m2!1snl!2snl!4v1633355448611!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
 </div>
 
