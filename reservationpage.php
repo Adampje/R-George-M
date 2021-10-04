498bf6383e1457dbde0bdea56da85a968504dd6d
MieuwPieuw <326758@student.mboutrecht.nl>
2021-10-04 23:27:53 +0200

    Sprint 1 fixed html en css
    Gefixed sprint 1

--- reservationpage.php
+++ reservationpage.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<h1 STYLE="font-size: 35px; font-family: , 'Reservations', Times, serif;">Reservations</h1>
+<h1 STYLE="font-size: 35px; font-family: Arial, 'Reservations', Times, serif;">Reservations</h1>
 
 <img src='img/GeorgeMarina.png' style='position:absolute; top:0; right:0;' width='100' height='100' alt='logoo' />
 
-  <hr style="height:5px;border-width:0;color:black;background-color:lightgrey">
+  <hr style="height:5px;border-width:0;color:black;background-color:white">
   <hr style="height:2px;border-width:0;color:black;background-color:black">
 
-  <a href="TableReservation.php">Reservate a table</a>
+<form action="TableReservation.php">
+    <input type="submit" value="Reservate a table" />
+</form>
 
-  <link rel="stylesheet" href="style.css">
+<form action="barreservation.php">
+    <input type="submit" value="Reservate a chair at the bar" />
+</form>
 
-
-  <a href="barreservation.php">Reservate a chair at the bar</a>
 
 </head>
 </html>
--- tablereservation.php
+++ tablereservation.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<body style="background-color:lightgrey;">
-<h1 STYLE="font-size: 35px; font-family: , 'Reservations', Times, serif;">Reservations</h1>
+<body style="background-color:whitesmoke;">
+<h1 STYLE="font-size: 35px; font-family: Arial, 'Reservations', Times, serif;">Reservations</h1>
 
 <img src='img/GeorgeMarina.png' style='position:absolute; top:0; right:0;' width='100' height='100' alt='logoo' />
 
 
-  <hr style="height:5px;border-width:0;color:black;background-color:lightgrey">
+<hr style="height:5px;border-width:0;color:black;background-color:whitesmoke">
   <hr style="height:2px;border-width:0;color:black;background-color:black">
 
-
-<h3> choose your seat </h3>
+  <h1 STYLE ="font-family: Arial;"> Choose your Table</h1>
 
 <a href ="reservationcomplete.php">
 
