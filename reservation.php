e4333838cdecec7b2df5bab8a0bf648c4018aab7
InfernoMa <75612582+InfernoMa@users.noreply.github.com>
2021-10-25 13:24:38 +0200

    Update reservation.php

--- reservation.php
+++ reservation.php
@@ -3,10 +3,8 @@
 <head>
 <h1 STYLE="font-size: 35px;  font-family: Arial, 'Reservations', Times, serif;">Reserveren</h1>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<img src='img/GeorgeMarina.png' style='position:absolute; top:0; right:0;' width='100' height='100' alt='logoo' />
 
-  <hr style="height:30px;border-width:0;color:black;background-color:white">
-  <hr style="height:2px;border-width:0;color:black;background-color:black">
+    <br>
 
   <link rel="stylesheet" href="menu.css">
 
