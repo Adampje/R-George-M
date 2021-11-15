312ccf66648b78f37d84ce02cb2ce4714195fec6
BerkanHRGL <329918@student.mboutrecht.nl>
2021-11-15 14:44:36 +0100

    New Navbar

--- navbar.php
+++ navbar.php
@@ -2,18 +2,6 @@
 <?php include 'navbar.css'; ?>
 </style>
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-   
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-    <link rel="shortcut icon" href="./img/icons/favicon.ico" type="image/x-icon">
-</head>
-
 <?php
   $active = (isset($_GET["content"]))?  $_GET["content"]: "";
 ?>
