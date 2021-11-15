1e1f402c7ee2c5bc24b6b513abef34373bad10a5
MieuwPieuw <326758@student.mboutrecht.nl>
2021-11-15 16:22:43 +0100

    Book event
    Bookingpage mooier mooier gemaakt

--- bookingpage.php
+++ bookingpage.php
@@ -3,18 +3,49 @@
 <head>
 <h1 STYLE="font-size: 35px; font-family: Arial, 'Reservations', Times, serif;">Book event</h1>
 
+<<<<<<< Updated upstream
 <img src='img/GeorgeMarina2.png' style='position:absolute; top:0; right:0;' width='100' height='100' alt='logoo'>
 
   <hr style="height:10px;border-width:0;color:black;background-color:white">
   <hr style="height:2px;border-width:0;color:black;background-color:black">
+=======
+<br>
+<br>
+<h1 STYLE="font-size: 35px; text-align: center; font-family: Comic Sans MS, 'Reservations', Times, serif;">Book Event</h1>
+<link rel="stylesheet" href="menu.css">
+<br>
+<br>
+>>>>>>> Stashed changes
 
-<p STYLE="font-family: Arial;"> In search of an inspiring, creative meeting space that's all about quality,</p>
-<p STYLE="font-family: Arial;">comfort and a relaxed atmosphere? We're here to make it happen for you. </p>
-<p STYLE="font-family: Arial;">With our great and well trained best of the best staff, we from George Marina </p>
-<p STYLE="font-family: Arial;">can promise you a guaranteed succesful fancy lunch or dinner.</p>
 
+<p STYLE="font-family: Comic Sans MS; text-align: center;">Op zoek naar een inspirerende, creative meeting space die draait om kwaliteit,</p>
+<p STYLE="font-family: Comic Sans MS; text-align: center;">comfort en een relaxte sfeer? Wij maken dit mogelijk voor u. </p>
+<p STYLE="font-family: Comic Sans MS; text-align: center;">Voor bedrijfsuitjes en grote groepen, staan wij van George Marina </p>
+<p STYLE="font-family: Comic Sans MS; text-align: center;">voor u klaar voor een gegarandeerde en succesvole lunch of diner.</p>
+
+<<<<<<< Updated upstream
 <H3 STYLE="font-family: Arial;"> Questions </h3>
 <a STYLE="font-family: Arial;" href= "contact.php">Here you can find information on how to contact our hosts!</a>
+=======
+<div class="questions">
+<H3 STYLE="font-family: Comic Sans MS;"> Questions </h3>
+
+<a STYLE="font-family: Comic Sans MS;" href= "./index.php?content=contact">Here you can find information on how to contact our hosts!</a>
+</div>
+
+<div class="bottomright">
+ <a href="https://www.instagram.com/georgeamsterdam/" target="_blank">
+  <img src="img\instagram.jpg" />
+ </a>
+ <a href="https://www.facebook.com/pages/category/Restaurant/George-Amsterdam-997786783679032/" target="_blank">
+  <img src="img/facebook.png" />
+  
+ </a>
+</div>  
+
+
+
+>>>>>>> Stashed changes
 
 </head>
 </html>
