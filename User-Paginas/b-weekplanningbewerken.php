3622de3b0e7eaf30d83891d5fadaecad4810ef77
callofdeath <60213819+callofdeath@users.noreply.github.com>
2021-12-14 15:35:21 +0100

    add file b-weekplanningbewerken.php
    added file b-weekplanningbewerken.php
    b-weekplanning template complete

---
+++ User-Paginas/b-weekplanningbewerken.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+
+        #footer{
+             display: none;
+        }
+
+        h1 {
+            position: relative;
+            text-align: center;
+        }
+
+        p {
+            position: relative;
+            text-align: center;
+        }
+
+        div {
+            position: relative;
+            text-align: center;
+            margin: auto;   
+        }
+
+        input {
+            width: calc(30% - 10px);
+            padding: 5px;
+        }
+
+        select {
+            width: calc(30% - 10px);
+            padding: 7px 0;
+        }
+
+        button {
+            width: calc(30% - 10px);
+            
+        }
+        
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="./index.php?content=carreer_script" method="post">
+    <div class="container mt-5">
+        <div class="center col-12 col-sm-6 name-item">
+        <tr>
+            <th scope="col">id</th>
+            <th scope="col">Datum</th>
+            <th scope="col">Tijd</th>
+            <th scope="col">Vak</th>
+            <th scope="col">Commentaar</th>
+          </tr> 
+        </div>
+        <br>
+        <div class="center col-12 col-sm-6 name-item">
+            <label for="InputEmail" class="form-label"></label>
+            <input name="email" type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="E-mailadress*" required autofocus/>
+            <div id="emailHelp" class="form-text text-muted"></div>
+        </div>
+        <br>
+        <div class="center col-12 col-sm-6 name-item">
+            <label for="InputTelefoonnummer" class="form-label"></label>
+            <input name="telefoonnummer" type="name" class="form-control" id="InputTelefoonnummer" aria-describedby="telefoonnummerHelp" placeholder="Telefoonnummer*" required autofocus/>
+            <div id="telefoonnummerHelp" class="form-text text-muted"></div>
+        </div>
+        <br>
+        <div class="position-item">
+            <div class="center col-12 col-sm-6 name-item">
+                <p style="margin:auto;">Voor welke positie soliciteer je?<span class="required">*</span></p>
+                <br>
+                <label for="InputPositie" class="form-label"></label>
+
+            </div>
+            <br>
+
+            <div class="btn-block">
+                <button type="nav-link" >Opslaan</button>
+
+            </div>
+    </form>
+    </div>
+
+    <br>
+
+</body>
+
+</html>
